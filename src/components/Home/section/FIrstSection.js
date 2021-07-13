1b954206b147d2a83e2079a565398ecadd7c223c
manishkeltton <manish.kumar3@kelltontech.com>
2021-07-13 19:46:38 +0530

    Party Celebration App

---
+++ src/components/Home/section/FIrstSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FIrstSection = () => {
+  return (
+    <div className="container-fluid">
+      <div class="row mt-3">
+        <div class="col">
+          <img src="https://deowgxgt4vwfe.cloudfront.net/uploads/1613554956_original.jpg" width="100%"/>
+        </div>
+        <div class="col">
+          <img src="https://deowgxgt4vwfe.cloudfront.net/uploads/1592147697_original.jpg" width="100%" />
+        </div>
+        <div class="col">
+          <img src="https://deowgxgt4vwfe.cloudfront.net/uploads/1618641415_original.jpg" width="100%" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FIrstSection;
